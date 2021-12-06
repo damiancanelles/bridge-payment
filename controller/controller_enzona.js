a25a55b100d597e6bab5be881681c166d56c0fdc
Damian <dcanellesgomez@gmail.com>
2021-12-06 18:14:33 -0500

    changes

--- controller/controller_enzona.js
+++ controller/controller_enzona.js
@@ -43,7 +43,52 @@ const ENZONA_bridge = async (req, res) => {
     }
 }
 
+const ENZONA_bridge_unbody = async (req, res) => {
+    try {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        const {url, credential} = req.body;
+        const data = `${credential.key}:${credential.secret}`;
+        const buff = Buffer.from(data, "utf8");
+        const base64data = buff.toString('base64');
+        const params = new URLSearchParams()
+        params.append('grant_type', 'client_credentials')
+        params.append('scope', 'am_application_scope enzona_business_payment')
+        const config1 = {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `Basic ${base64data}`
+            }
+          }
+         
+        await axios.post('https://apisandbox.enzona.net/token',params,config1)
+        .then(async (result) => {
+            const config2 = {
+                headers: {
+                    'Authorization':`Bearer ${result.data.access_token}`
+                }
+              } 
+            console.log(config2)
+            await axios.post(`https://apisandbox.enzona.net/payment/v1.0.0/payments/9e0942c10157494383326ff79709f234/complete`,{ headers: {'Authorization': 'Bearer 806ac2b5-eeca-3948-814d-409bc940ec77'}})
+            .then((result) => {
+                console.log(result)
+                res.json(result.data)
+            
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+        })
+        .catch((err) => {
+            res.json(err)
+        })  
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
 
 module.exports = {
-    ENZONA_bridge
+    ENZONA_bridge,
+    ENZONA_bridge_unbody
 }
--- routes/enzona_route.js
+++ routes/enzona_route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {ENZONA_bridge} = require("../controller/controller_enzona")
+const {ENZONA_bridge,ENZONA_bridge_unbody} = require("../controller/controller_enzona")
 
 router.post("/", ENZONA_bridge);
+router.post("/unbody", ENZONA_bridge_unbody);
 
 module.exports = router;
